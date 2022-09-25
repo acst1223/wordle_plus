07bca2385e5cfcc77337b7afd90fc6d579779f3d
acstAcMac <natnstart@gmail.com>
2022-09-25 16:41:36 +0800

    input validity judge

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 let langGame = "en";
-let wordData = null;
+let wordLength = 6;
+let words = null; // all words
+let alphabet = null; // alphabet
 
 let firstCell = document.querySelector("div.cell");
 let main = document.querySelector("main");
+let inputBox = document.querySelector("input.main");
+let button = document.querySelector("button.main");
+
+/**
+ * Load data from dicts.
+ */
 
 function loadData() {
   fetch(`dicts/${langGame}.json`)
     .then((response) => response.json())
     .then((data) => {
-      wordData = data;
+      words = data.words;
+      let s = new Set();
+      for (let i = 0; i < data.alphabet.length; i++) {
+        s.add(data.alphabet[i]);
+      }
+      alphabet = s;
     });
 }
 loadData();
 
-setTimeout(() => {
-  console.log("wordData", wordData);
-}, 100);
+/**
+ * Set cell font size.
+ * Reset when resizing window.
+ */
 
 function setCellFontSize() {
   let cellSize = firstCell.clientHeight;
@@ -24,4 +38,33 @@ function setCellFontSize() {
 setCellFontSize();
 addEventListener("resize", () => {
   setCellFontSize();
+});
+
+/**
+ * Check input validity.
+ */
+
+function checkInput(input) {
+  if (alphabet === null || input.length != wordLength) {
+    return false;
+  }
+  for (let i = 0; i < input.length; i++) {
+    if (!alphabet.has(input[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+inputBox.addEventListener("input", () => {
+  let v = inputBox.value;
+  if (langGame == "en") {
+    v = v.toUpperCase();
+  }
+  if (checkInput(v)) {
+    button.classList.add("valid");
+  } else {
+    button.classList.remove("valid");
+  }
+  console.log(v, checkInput(v));
 });
