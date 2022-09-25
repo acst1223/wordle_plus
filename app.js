57112b528f2618d607732dbeda73032c55b55fee
acstAcMac <natnstart@gmail.com>
2022-09-25 15:22:10 +0800

    dynamic font size

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let langGame = "en";
 let wordData = null;
 
+let firstCell = document.querySelector("div.cell");
+let main = document.querySelector("main");
+
 function loadData() {
   fetch(`dicts/${langGame}.json`)
     .then((response) => response.json())
@@ -13,3 +16,12 @@ loadData();
 setTimeout(() => {
   console.log("wordData", wordData);
 }, 100);
+
+function setCellFontSize() {
+  let cellSize = firstCell.clientHeight;
+  main.style.fontSize = `${cellSize * 0.6}px`;
+}
+setCellFontSize();
+addEventListener("resize", () => {
+  setCellFontSize();
+});
